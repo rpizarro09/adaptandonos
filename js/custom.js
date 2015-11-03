0eae1e90e80996f782b2ba6704235816316bb117
Rodrigo Pizarro <rpizarro09@gmail.com>
2015-11-03 00:39:40 -0300

    version 3

--- js/custom.js
+++ js/custom.js
@@ -69,17 +69,6 @@ jQuery(function($){
     });
 
 
-	/* ----------------------------------------------------------- */
-	/*  4. Skill Circle
-	/* ----------------------------------------------------------- */
-
-	$('#myStathalf').circliful();
-	$('#myStat').circliful();
-	$('#myStathalf2').circliful();
-	$('#myStat2').circliful();
-	$('#myStat3').circliful();
-	$('#myStat4').circliful();
-	$('#myStathalf3').circliful();
 
 
 	/* ----------------------------------------------------------- */
